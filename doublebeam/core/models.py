6847b3c329a196245daea81a1c4f7be0ea9383e1
Darius Arnold <mail@dariusarnold.de>
2019-04-29 22:41:49 +0200

    Add LinearVelocityLayer which has linear gradients in its properties

--- doublebeam/core/models.py
+++ doublebeam/core/models.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-VelocityLayer = np.dtype([
+# Layer which has a constant velocity over its depth
     ('top_depth', np.float_),
     #('bot_depth', np.float_),
     ('top_p_velocity', np.float_),
@@ -12,24 +12,75 @@ VelocityLayer = np.dtype([
     #('bot_density', np.float_),
 ])
 
+# Layer which has a linear velocity gradient over its depth
+LinearVelocityLayer = np.dtype([
+    ('top_depth', np.float_),
+    ('bot_depth', np.float_),
+    ('top_p_velocity', np.float_),
+    ('bot_p_velocity', np.float_),
+    ('top_s_velocity', np.float_),
+    ('bot_s_velocity', np.float_),
+    ('top_density', np.float_),
+    ('bot_density', np.float_),
+])
 
-def evaluate_at(layer: VelocityLayer, depth: float, prop: str) -> float:
+
+def evaluate_at(layer: ConstantVelocityLayer, prop: str) -> float:
     """
-    Evaluate material properties at a given depth in a velocity layer
+    Evaluate material properties of a velocity layer
     :param layer: The layer to evaluate
-    :param depth: At which depth to evaluate the layer
     :param prop: Which property to evaluate.
     "p" = P wave velocity (km/s)
     "s" = S wave velocity (km/s)
-    "r" or "d" = density (gm/cm³)
+    "r" or "d" = density (gm/cm^3)
     :return: The requested layer property
     """
+
     if prop == "p":
-        return layer["top_density"]
+        key = "top_density"
     elif prop == "s":
-        return layer["top_s_velocity"]
+        key = "top_s_velocity"
     elif prop in "rd":
-        return layer["top_density"]
+        key = "top_density"
+    return layer[key]
+
+
+def evaluate_at_linear(layer: LinearVelocityLayer, depth: float, prop: str) -> float:
+    """
+    Evaluate material properties at a given depth in a layer with linearly
+    varying properties
+    :param layer: The layer to evaluate
+    :param depth: Depth in km at which to evaluate the property
+    :param prop: Which property to evaluate
+    "p" = P wave velocity (km/s)
+    "s" = S wave velocity (km/s)
+    "r" or "d" = density (gm/cm^3)
+    :return: The requested layer property
+    """
+
+    def eval_linear_gradient(x_left: float, x_right: float, y_left: float,
+                             y_right: float, x_position: float) -> float:
+        """
+        Used for interpolation between the layers property at top and bottom.
+        The gradient is given by two points, which have an x value and a
+        corresponding y value. Linear interpolation between the two points at the
+        position given by value is used for the result
+        :return: Value of linear gradient at x_position
+        """
+        slope = (y_right - y_left) / (x_right - x_left)
+        return y_left + slope * x_position
+
+    if prop == "p":
+        top_key, bottom_key = "top_p_velocity", "bot_p_velocity"
+    elif prop == "s":
+        top_key, bottom_key = "top_s_velocity", "bot_s_velocity"
+    elif prop in "rd":
+        top_key, bottom_key = "top_density", "bot_density"
+
+    top_value, bottom_value = layer[top_key], layer[bottom_key]
+    return eval_linear_gradient(layer["top_depth"], layer["bot_depth"],
+                                top_value, bottom_value, depth)
+
 
 
 class VelocityModel1D:
@@ -40,7 +91,7 @@ class VelocityModel1D:
         self.layers = layers
 
     @classmethod
-    def from_file(cls, filepath):
+    def from_file(cls, filepath: Path):
         raw_data = np.loadtxt(filepath, dtype=VelocityLayer, delimiter=",")
         return cls(raw_data)
 
@@ -55,16 +106,17 @@ class VelocityModel1D:
         # the given depth
         layer = np.where(layer)[0]
         if len(layer):
-            # return the layer which top depths is directly above depth_km
+            # return the layer which top depth is directly above depth_km
             return layer[-1]
         else:
-            raise LookupError(f"No layer found at depth {depth_km}")
-
+            raise LookupError(f"No layer found in model at depth {depth_km}")
 
     def eval_at(self, depth_km: float, prop: str) -> float:
         layer = self.layers[self.layer_number(depth_km)]
-        return evaluate_at(layer, depth_km, prop)
-
+        if layer.dtype == ConstantVelocityLayer:
+            return evaluate_at(layer, prop)
+        elif layer.dtype == LinearVelocityLayer:
+            return evaluate_at_linear(layer, depth_km, prop)
 
     def __len__(self):
         return len(self.layers)
