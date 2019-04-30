85465835c78fdb599a64df3ddc24f15753b35f09
Darius Arnold <mail@dariusarnold.de>
2019-04-30 16:45:30 +0200

    Rewrite tests for VelocityModel

--- tests/test_velocity_model.py
+++ tests/test_velocity_model.py
@@ -1,68 +1,102 @@
 import unittest
 
 import numpy as np
+from numpy.testing import assert_array_equal
 from utils import TempFile
 
-from doublebeam.core.models import LinearVelocityLayer, ConstantVelocityLayer, VelocityModel1D, evaluate_at_linear
+from doublebeam.core.models import LinearVelocityLayer, ConstantVelocityLayer, VelocityModel1D
 
 
-class TestLinearVelocityLayer(unittest.TestCase):
-
-    def test_evaluation_of_layer(self):
-        layer = np.array((0, 10, 3., 6., 2., 4., 2.5, 3.5), dtype=LinearVelocityLayer)
-        p_vel = evaluate_at_linear(layer, 5, prop="p")
-        self.assertEqual(p_vel, 4.5)
-
-
-class TestLoadVelocityModel1DLinearVelocity(unittest.TestCase):
+class TestVelocityModel1DLinearLayers(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         # create temp file with example model data
-        content = """#Depth(km), vp (km/s), vs (km/s), density (g/cm^3)
-                     0.00,5.8000,3.3600,2.700
-                     ,5.8000,3.4,2750
-                     10.00,5.8000,3.3600,2.750
-                     ,6.0,4.0,2.75
-                     35.00,8.0400,4.4700,2.800"""
+        content = """#Depth top(km), Depth bottom (km), vp top (km/s), bp bottom (km/s), vs top (km/s), vs bottom (km/s), density top (g/cm^3), density bottom (g/cm^3)
+                     0.00, 10., 5.8000, 5.8, 3.3600, 3.4, 2.700, 2.75
+                     10.00, 35, 5.9000, 6.0, 3.4, 4.0, 2.750, 2.9"""
         cls.path, cls.file = TempFile(content)
+        # manually parsed to compare
+        cls.layers = np.array([(0., 10., 5.8, 5.8, 3.36, 3.4, 2.7, 2.75),
+                               (10., 35., 5.9, 6., 3.4, 4., 2.75, 2.9)], dtype=LinearVelocityLayer)
 
-    def test_file_load_success(self):
-        pass
+    @classmethod
+    def tearDownClass(cls):
+        cls.file.close()
+
+    def setUp(self):
+        self.v = VelocityModel1D(self.layers)
+
+    def test_init(self):
+        """Test if creating a VelocityModel populates the data structure correctly"""
+        assert_array_equal(self.v.layers, self.layers)
+
+    def test_file_load(self):
+        loaded_model = VelocityModel1D.from_file(self.path)
+        assert_array_equal(loaded_model.layers, self.v.layers)
+
+    def test_prop_access_success(self):
+        msg = "{} evaluation failed or incorrect"
+        self.assertEqual(self.v.eval_at(0, "d"), 2.7, msg=msg.format("Density"))
+        self.assertEqual(self.v.eval_at(5, "s"), 3.38, msg=msg.format("S velocity"))
+        self.assertEqual(self.v.eval_at(1, "p"), 5.8, msg=msg.format("P velocity"))
+
+    def test_layer_top_inclusivity(self):
+        # since upper boundary is inclusive, value of lower layer should be
+        # returned here
+        self.assertEqual(self.v.eval_at(10., "p"),  5.9)
+
+    def test_raises_when_depth_out_of_model_range(self):
+        with self.assertRaises(LookupError, msg="Model evaluation at negative depth not raising correct exception"):
+            self.v.eval_at(-5.5, "p")
+        with self.assertRaises(LookupError, msg="Model evaluation below lowest layer not raising correct exception"):
+            self.v.eval_at(999.9, "p")
 
 
-class TestLoadVelocityModel1D(unittest.TestCase):
+class TestVelocityModel1DConstantLayers(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         # create temp file with example model data
         content = """#Depth(km), vp (km/s), vs (km/s), density (g/cm^3)
-                     0.00,10.,5.8000,3.3600,2.700
-                     10.00.35.,5.8000,3.3600,2.750
-                     35.00,50.,8.0400,4.4700,2.800"""
+                     0.00, 10., 5.8000, 3.3600, 2.700
+                     10.00, 35., 5.8000, 3.3600, 2.750
+                     35.00, 50., 8.0400, 4.4700, 2.800"""
         cls.path, cls.f = TempFile(content)
+        # manually parsed to compare
         cls.layers = np.array([(0, 10, 5.8, 3.36, 2.7), (10., 35, 5.8, 3.36, 2.75),
                                (35., 50, 8.04, 4.47, 2.8)], dtype=ConstantVelocityLayer)
 
     def setUp(self):
         self.v = VelocityModel1D(self.layers)
 
-    def test_file_load_success(self):
-        """
-        Test if loading from a file populates the data structure correctly
-        """
-        np.testing.assert_array_equal(self.v.layers, self.layers)
+    def test_init(self):
+        """Test if creating a VelocityModel populates the data structure correctly"""
+        assert_array_equal(self.v.layers, self.layers)
+
+    def test_file_load(self):
+        loaded_model = VelocityModel1D.from_file(self.path)
+        assert_array_equal(loaded_model.layers, self.v.layers)
 
     def test_prop_access_success(self):
         """
         Test successful access to the models properties
         """
-        self.assertEqual(self.v.eval_at(5, "r"), 2.7)
-        self.assertEqual(self.v.eval_at(555, "s"), 4.47)
+        msg = "{} evaluation failed or incorrect"
+        self.assertEqual(self.v.eval_at(5, "d"), 2.7, msg=msg.format("Density"))
+        self.assertEqual(self.v.eval_at(40, "s"), 4.47, msg=msg.format("S velocity"))
+        self.assertEqual(self.v.eval_at(20., "p"), 5.8, msg=msg.format("P velocity"))
 
-    def test_prop_access_out_of_range(self):
+    def test_layer_top_inclusivity(self):
+        # evaluation at layer border should yield value of lower layer
+        self.assertEqual(self.v.eval_at(35., "p"), 8.04)
+
+    def test_raises_when_depth_out_of_model_range(self):
         """
-        Test for correct exception when wrong depth is used
+        Test for correct exception when wrong depth (outside of models depth
+         range) is used
         """
-        with self.assertRaises(LookupError):
-            self.v.eval_at(-555, "p")
+        with self.assertRaises(LookupError, msg="Model evaluation at negative depth not raising correct exception"):
+            self.v.eval_at(-555, "p", )
+        with self.assertRaises(LookupError, msg="Model evaluation below lowest layer not raising correct exception"):
+            self.v.eval_at(999, "s")
