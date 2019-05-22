c81a19ac33b591b8378afff7f319d046e6303588
Darius Arnold <mail@dariusarnold.de>
2019-05-22 12:14:48 +0200

    Update tests for ray tracing replacing MockVelocityModel by Linear one
    Due to slight changes the result had to be updated as well

--- tests/test_ray_tracing.py
+++ tests/test_ray_tracing.py
@@ -1,7 +1,7 @@
 import unittest
 from math import radians
 
-from doublebeam.core.raytracing import ray_trace_euler, MockVelocityModel1D, Ray2D, ray_trace_scipy
+from doublebeam.core.raytracing import ray_trace_euler, VelocityModel1D, Ray2D, ray_trace_scipy
 
 
 class TestRayTracingEuler(unittest.TestCase):
@@ -11,7 +11,7 @@ class TestRayTracingEuler(unittest.TestCase):
         previously calculated. Changes in the structure of the function should
         not change the result."""
         last_point_expected = (9.298386897144244, -0.0029832008545231554)
-        vm = MockVelocityModel1D(1)
+        vm = VelocityModel1D.from_string("0, 1, 3, 4, 0, 0, 1, 1\n1, 101, 6, 156, 0, 0, 1, 1")
         ray = Ray2D(0, 0, radians(20))
         s_end = 20
         traced_points = ray_trace_euler(ray, vm, s_end)[-1]
@@ -25,8 +25,8 @@ class TestRayTracingScipy(unittest.TestCase):
         """This just tests if the last point of the ray is the same as
         previously calculated. Changes in the structure of the function should
         not change the result."""
-        last_point_expected = (6.506034140591608, -8.331009493378616e-16)
-        vm = MockVelocityModel1D(1)
+        last_point_expected = (6.458938131869513, -8.331009493378616e-16)
+        vm = VelocityModel1D.from_string("0, 1, 3, 4, 0, 0, 1, 1\n1, 101, 6, 156, 0, 0, 1, 1")
         ray = Ray2D(0, 0, radians(20))
         s_end = 20
         traced_points = ray_trace_scipy(ray, vm, s_end)[-1]
