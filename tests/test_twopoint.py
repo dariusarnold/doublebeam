7b25b55f82218ae28cbe83d9ac206e146b0cb1ae
Darius Arnold <mail@dariusarnold.de>
2019-07-25 10:12:23 +0200

    Rewrite two point ray tracing to class

--- tests/test_twopoint.py
+++ tests/test_twopoint.py
@@ -4,8 +4,7 @@ from math import radians, sin
 import numpy as np
 
 from doublebeam.core.models import VelocityModel3D
-from doublebeam.core.raytracing.twopoint import TwoPointRayTracing
-from twopoint_manual import trace
+from twopoint_manual import TwoPointRayTracing
 
 
 class TestTwoPointRayTracing(unittest.TestCase):
@@ -36,6 +35,7 @@ class TestNewTwoPointRayTracing(unittest.TestCase):
 
     def setUp(self) -> None:
         self.vm = VelocityModel3D.from_file("/home/darius/git/double-beam/fang2019model.txt")
+        self.rt = TwoPointRayTracing(self.vm)
         self.sources = [np.array((469/2, 0, 500)),
                         np.array((868/2, 0, 500)),
                         np.array((2159/2, 0, 500))]
@@ -46,7 +46,7 @@ class TestNewTwoPointRayTracing(unittest.TestCase):
     def test_correct_values(self):
         """Test if two point ray tracing gives the same slowness as calculated
         from fig. 9 in Fang2019"""
-        slownesses = [trace(source, receiver, self.vm) for source, receiver in zip(self.sources, self.receivers)]
+        slownesses = [self.rt.trace(source, receiver) for source, receiver in zip(self.sources, self.receivers)]
         slownesses_expected = [sin(radians(i)) / 3000 for i in (30, 50, 85)]
         for p, p_expected in zip(slownesses, slownesses_expected):
             with self.subTest(p=p, p_expected=p_expected):
@@ -54,7 +54,7 @@ class TestNewTwoPointRayTracing(unittest.TestCase):
 
     def test_source_receiver_swap(self):
         """Swapping receiver and source should give the same result"""
-        slownesses = [trace(receiver, source, self.vm) for source, receiver in zip(self.sources, self.receivers)]
+        slownesses = [self.rt.trace(receiver, source) for source, receiver in zip(self.sources, self.receivers)]
         slownesses_expected = [sin(radians(i)) / 3000 for i in (30, 50, 85)]
         for p, p_expected in zip(slownesses, slownesses_expected):
             with self.subTest(p=p, p_expected=p_expected):
