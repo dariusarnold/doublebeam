a0d1d8a9cbec20ad51f4a67cdbf71c627393a886
Darius Arnold <mail@dariusarnold.de>
2019-07-25 10:37:40 +0200

    Move two point ray tracing to proper file

--- tests/test_twopoint.py
+++ tests/test_twopoint.py
@@ -4,34 +4,10 @@ from math import radians, sin
 import numpy as np
 
 from doublebeam.core.models import VelocityModel3D
-from twopoint_manual import TwoPointRayTracing
+from doublebeam.core.raytracing.twopoint import TwoPointRayTracing
 
 
 class TestTwoPointRayTracing(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.vm = VelocityModel3D.from_file("/home/darius/git/double-beam/fang2019model.txt")
-        self.twopoint = TwoPointRayTracing(self.vm)
-
-    @unittest.skip
-    def test_theta_30_degrees(self):
-        """Test example ray from fig. 9. Slowness can be calculated from the
-        angle theta and the velocity using eq. 13"""
-        p = self.twopoint.trace((469., 0., 0), (469/2, 0., 500.))
-        #p = self.twopoint.trace((469 / 2, 0., 500.), (469., 0., 0))
-        p_correct = sin(radians(30)) / 3000
-        self.assertAlmostEqual(p, p_correct, places=5)
-
-    @unittest.skip
-    def test_theta_50_degree(self):
-        """Test example ray from fig. 9. Slowness cam be calcilated from the
-        angle theta and the velocity using eq. 13"""
-        p = self.twopoint.trace((434, 0, 500), (868, 0, 0))
-        p_correct = sin(radians(50)) / 3000
-        self.assertAlmostEqual(p, p_correct, places=5)
-
-
-class TestNewTwoPointRayTracing(unittest.TestCase):
 
     def setUp(self) -> None:
         self.vm = VelocityModel3D.from_file("/home/darius/git/double-beam/fang2019model.txt")
