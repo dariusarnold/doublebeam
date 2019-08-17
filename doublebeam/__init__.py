37612378d0d085ff0b16dc7bb652cd12b790406a
Darius Arnold <mail@dariusarnold.de>
2019-08-17 18:15:38 +0200

    Add common funcitonality as exported symbols to top level doublebeam package

---
+++ doublebeam/__init__.py
@@ -0,0 +1,5 @@
+from doublebeam.raytracing.ray import Ray3D, GaussBeam
+from doublebeam.raytracing.initial_value import KinematicRayTracer3D, DynamicRayTracer3D
+from doublebeam.raytracing.twopoint import TwoPointRayTracing
+from doublebeam.doublebeam import DoubleBeam, FractureParameters
+from doublebeam.models import VelocityModel3D
