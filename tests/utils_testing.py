f8145c7aec6b23e7ebe3159518804f257b2436af
Darius Arnold <mail@dariusarnold.de>
2019-08-19 10:37:11 +0200

    Add custom TestCase class providing two methods to compare numpy arrays
    Add type comparison for arrays to self.assertEqual and provide asssertAllClose that delegates to numpy function.

--- tests/utils_testing.py
+++ tests/utils_testing.py
@@ -1,6 +1,9 @@
+import unittest
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import Tuple, TextIO
+
+import numpy as np
 
 
 def TempFile(content: str) -> Tuple[Path, TextIO]:
@@ -14,3 +17,26 @@ def TempFile(content: str) -> Tuple[Path, TextIO]:
     f.write(content)
     f.flush()
     return Path(f.name), f
+
+
+class TestCase(unittest.TestCase):
+    """
+    Custom TestCase that delegates array asserts to the corresponding numpy
+    functions.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.addTypeEqualityFunc(np.ndarray, self._array_equal)
+
+    @staticmethod
+    def _array_equal(x, y, msg=None):
+        # unittest passes None for empty message, but numpy expects empty string
+        msg = "" if msg is None else msg
+        np.testing.assert_array_equal(x, y, err_msg=msg)
+
+    def assertAllClose(self, actual: np.ndarray, desired: np.ndarray, rtol: float = 1E-7,
+                       atol: float = 0, msg: str = None):
+        # unittest passes None for empty message, but numpy expects empty string
+        msg = "" if msg is None else msg
+        np.testing.assert_allclose(actual, desired, rtol, atol, err_msg=msg)
