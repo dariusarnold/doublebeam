bf5d20a7e17ac371f7d308bb6fc12202b101151c
Darius Arnold <mail@dariusarnold.de>
2019-08-26 18:02:11 +0200

    Stash all unstaged changes before running unittests in pre-commit hook
    This way only the commit is tested, not the full working tree.

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-# Run unittests as pre-commit hook. Symink this script
+# Run unittests as pre-commit hook. Symlink this script
 # into your .git/hooks as pre-commit to have tests run
 # automatically before committing.
 # cd double-beam
 # ln -s ../../scripts/pre-commit.sh .git/hooks/pre-commit
 
 echo "Running pre-commit hook"
+# save other changes as stash so only the commit is tested
+STASH_NAME="pre-commit-$(date +%s)"
+git stash save -q --keep-index --include-untracked "$STASH_NAME"
 ./scripts/run-unittests.sh
-
-if [ $? -ne 0 ]; then
+UNITTESTS_EXIT_CODE=$?
+# unstash
+git stash pop -q
+if [ $UNITTESTS_EXIT_CODE -ne 0 ]; then
 	echo "Tests must pass before commit"
 	exit 1
 fi
