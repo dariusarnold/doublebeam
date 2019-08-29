f91149e46fddea55c269515abc5dfc323a511287
Darius Arnold <mail@dariusarnold.de>
2019-08-29 22:08:26 +0200

    Split integration.cpp into source and header

--- doublebeam-cpp/src/integration.cpp
+++ doublebeam-cpp/src/integration.cpp
@@ -1,10 +1,9 @@
 #include <boost/numeric/odeint.hpp>
+
+#include "integration.hpp"
 #include "model.h"
 #include "utils.h"
 
-namespace odeint = boost::numeric::odeint;
-
-typedef std::array<double, 7> state_type;
 
 class KinematicRayTracingEquation {
     const VelocityModel& vm;
@@ -35,6 +34,7 @@ public:
     }
 };
 
+
 struct Index {
     /*
      * Indices of variables in state_type.
@@ -50,6 +50,7 @@ struct Index {
     static constexpr size_t PZ = 5;
     static constexpr size_t T = 6;
 };
+
 
 struct InterfaceCrossed {
     /*
@@ -67,80 +68,9 @@ struct InterfaceCrossed {
 };
 
 
-struct store_solver_state {
-    std::vector<state_type>& states;
-    std::vector<double>& times;
-
-    store_solver_state(std::vector<state_type>& states, std::vector<double>& times) : states(states), times(times) {}
-
-    void operator()(const state_type& x, double t) {
-        states.push_back(x);
-        times.push_back(t);
-    }
-};
-
-
 state_type init_state(double x, double y, double z, const VelocityModel& model,
                       double theta, double phi, double T = 0) {
     const double velocity = model.eval_at(z);
     const auto[px, py, pz] = seismo::slowness_3D(theta, phi, velocity);
     return {x, y, z, px, py, pz, T};
-}
-
-
-/**
- * Integrate a system of ODEs until the Condition has a zero crossing.
- * @tparam System Type of odeint System, callable with signature  sys(x, dxdt, t)-
- * @tparam Condition Type of callable taking a state_type and returning a double.
- * @param x0 Initial state of the system.
- * @param sys System to be integrated.
- * @param cond Return continuous function with a zero crossing for increasing time.
- * @param s_start Start arclength of integration.
- * @param ds Step size of integration.
- * @param precision Precision for zero crossing search. The width of the arclength window
- * in which the zero crossing occurs is decreased until it is smaller than precision.
- * @param max_ds Maximum time step size.
- * @return
- */
-template<typename System, typename Condition>
-std::vector<std::pair<double, state_type>>
-find_crossing(state_type& x0, System sys, Condition cond, const double s_start,
-              const double ds, const double precision = 1.E-6, double max_ds = 1.1) {
-    auto stepper = odeint::make_dense_output(1.E-6, 1.E-6, max_ds, odeint::runge_kutta_dopri5<state_type>());
-    stepper.initialize(x0, s_start, ds);
-
-    // advance stepper until first sign change occurs
-    std::vector<std::pair<double, state_type>> states;
-    double current_cond = cond(stepper.current_state());
-    do {
-        states.emplace_back(stepper.current_time(), stepper.current_state());
-        stepper.do_step(sys);
-    } while (math::same_sign(cond(stepper.current_state()), current_cond));
-
-    // find exact point of zero crossing
-    double t0 = stepper.previous_time();
-    double t1 = stepper.current_time();
-    double t_middle;
-    state_type x_middle;
-    while (std::abs(t1 - t0) > precision) {
-        t_middle = (t0 + t1) * 0.5;
-        stepper.calc_state(t_middle, x_middle);
-        if (cond(x_middle) == 0) {
-            // midpoint found
-            break;
-        } else if (cond(x_middle) > 0) {
-            // we are above the interface crossing, condition change
-            // lies below the midpoint
-            t0 = t_middle;
-        } else {
-            // below interface crossing, condition change lies above
-            // the midpoint
-            t1 = t_middle;
-        }
-    }
-    // interval of size precision; take its midpoint as a final guess
-    t_middle = (t0 + t1) * 0.5;
-    stepper.calc_state(t_middle, x_middle);
-    states.emplace_back(t_middle, x_middle);
-    return states;
 }
---
+++ doublebeam-cpp/src/integration.hpp
@@ -0,0 +1,76 @@
+#ifndef DOUBLEBEAM_CPP_INTEGRATION_HPP
+#define DOUBLEBEAM_CPP_INTEGRATION_HPP
+
+
+#include <vector>
+#include <array>
+
+#include <boost/numeric/odeint/stepper/runge_kutta_dopri5.hpp>
+#include <boost/numeric/odeint/stepper/generation/make_dense_output.hpp>
+#include <boost/numeric/odeint/stepper/generation/generation_runge_kutta_dopri5.hpp>
+
+#include "utils.h"
+
+typedef std::array<double, 7> state_type;
+
+namespace odeint = boost::numeric::odeint;
+
+/**
+ * Integrate a system of ODEs until the Condition has a zero crossing.
+ * @tparam System Type of odeint System, callable with signature  sys(x, dxdt, t)-
+ * @tparam Condition Type of callable taking a state_type and returning a double.
+ * @param x0 Initial state of the system.
+ * @param sys System to be integrated.
+ * @param cond Return continuous function with a zero crossing for increasing time.
+ * @param s_start Start arclength of integration.
+ * @param ds Step size of integration.
+ * @param precision Precision for zero crossing search. The width of the arclength window
+ * in which the zero crossing occurs is decreased until it is smaller than precision.
+ * @param max_ds Maximum time step size.
+ * @return
+ */
+template<typename System, typename Condition>
+std::vector<std::pair<double, state_type>>
+find_crossing(state_type& x0, System sys, Condition cond, const double s_start,
+              const double ds, const double precision = 1.E-6, double max_ds = 1.1) {
+    auto stepper = odeint::make_dense_output(1.E-6, 1.E-6, max_ds, odeint::runge_kutta_dopri5<state_type>());
+    stepper.initialize(x0, s_start, ds);
+
+    // advance stepper until first sign change occurs
+    std::vector<std::pair<double, state_type>> states;
+    double current_cond = cond(stepper.current_state());
+    do {
+        states.emplace_back(stepper.current_time(), stepper.current_state());
+        stepper.do_step(sys);
+    } while (math::same_sign(cond(stepper.current_state()), current_cond));
+
+    // find exact point of zero crossing
+    double t0 = stepper.previous_time();
+    double t1 = stepper.current_time();
+    double t_middle;
+    state_type x_middle;
+    while (std::abs(t1 - t0) > precision) {
+        t_middle = (t0 + t1) * 0.5;
+        stepper.calc_state(t_middle, x_middle);
+        if (cond(x_middle) == 0) {
+            // midpoint found
+            break;
+        } else if (cond(x_middle) > 0) {
+            // we are above the interface crossing, condition change
+            // lies below the midpoint
+            t0 = t_middle;
+        } else {
+            // below interface crossing, condition change lies above
+            // the midpoint
+            t1 = t_middle;
+        }
+    }
+    // interval of size precision; take its midpoint as a final guess
+    t_middle = (t0 + t1) * 0.5;
+    stepper.calc_state(t_middle, x_middle);
+    states.emplace_back(t_middle, x_middle);
+    return states;
+}
+
+
+#endif //DOUBLEBEAM_CPP_INTEGRATION_HPP
