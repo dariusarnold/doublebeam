946b3c1ced023f1220bddc756cc93e28f9357edb
Darius Arnold <mail@dariusarnold.de>
2019-08-30 14:24:38 +0200

    Modify python unittest run script

--- scripts/run-unittests.sh
+++ scripts/run-unittests.sh
@@ -4,8 +4,10 @@
 
 # exit immediately if a command fails
 trap 'exit' ERR
-
-source activate py36
+# fix for conda activate not working in bash script 
+# see https://github.com/conda/conda/issues/7980
+eval "$(conda shell.bash hook)"
+conda activate py37
 #echo `"${0%/*}/.."`
 cd "${0%/*}/.."
 python -m unittest discover -s tests
