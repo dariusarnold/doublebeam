9b011358018f42df54e987aa828d624944a4cdc1
Darius Arnold <mail@dariusarnold.de>
2019-09-02 14:20:44 +0200

    Replace interval halfing by TOMS748 algorithm for calculating position of interface crossing

--- doublebeam-cpp/src/integration.hpp
+++ doublebeam-cpp/src/integration.hpp
@@ -8,6 +8,7 @@
 #include <boost/numeric/odeint/stepper/runge_kutta_dopri5.hpp>
 #include <boost/numeric/odeint/stepper/generation/generation_runge_kutta_dopri5.hpp>
 #include <boost/numeric/odeint/stepper/generation/generation_dense_output_runge_kutta.hpp>
+#include <boost/math/tools/toms748_solve.hpp>
 
 #include "utils.h"
 
@@ -24,15 +25,12 @@ namespace odeint = boost::numeric::odeint;
  * @param cond Return continuous function with a zero crossing for increasing time.
  * @param s_start Start arclength of integration.
  * @param ds Step size of integration.
- * @param precision Precision for zero crossing search. The width of the arclength window
- * in which the zero crossing occurs is decreased until it is smaller than precision.
  * @param max_ds Maximum time step size.
  * @return
  */
 template<typename System, typename Condition>
 std::pair<std::vector<double>, std::vector<state_type>>
-find_crossing(state_type& x0, System sys, Condition cond, double s_start, double ds,
-              double max_ds = 1.1, double precision = 1E-6) {
+find_crossing(state_type& x0, System sys, Condition cond, double s_start, double ds, double max_ds = 1.1) {
     auto stepper = odeint::make_dense_output(1.E-6, 1.E-6, max_ds, odeint::runge_kutta_dopri5<state_type>());
     stepper.initialize(x0, s_start, ds);
     std::vector<double> arclengths;
@@ -48,26 +46,13 @@ find_crossing(state_type& x0, System sys, Condition cond, double s_start, double
     // find exact point of zero crossing
     double t0 = stepper.previous_time();
     double t1 = stepper.current_time();
-    double t_middle;
     state_type x_middle;
-    while (std::abs(t1 - t0) > precision) {
-        t_middle = (t0 + t1) * 0.5;
-        stepper.calc_state(t_middle, x_middle);
-        if (cond(x_middle) == 0) {
-            // midpoint found
-            break;
-        } else if (cond(x_middle) > 0) {
-            // we are above the interface crossing, condition change
-            // lies below the midpoint
-            t0 = t_middle;
-        } else {
-            // below interface crossing, condition change lies above
-            // the midpoint
-            t1 = t_middle;
-        }
-    }
-    // interval of size precision; take its midpoint as a final guess
-    t_middle = (t0 + t1) * 0.5;
+    boost::uintmax_t max_calls = 1000;
+    auto [t_left, t_right] = boost::math::tools::toms748_solve([&](double t){
+        stepper.calc_state(t, x_middle); return (cond(x_middle));}, t0, t1,
+                boost::math::tools::eps_tolerance<double>(), max_calls);
+    // calculate final position of crossing and save state at crossing
+    auto t_middle = (t_left + t_right) * 0.5;
     stepper.calc_state(t_middle, x_middle);
     arclengths.emplace_back(t_middle);
     states.emplace_back(x_middle);
