dd1bb9b6e1d2954b37f3cf6e51786157c642a978
Darius Arnold <mail@dariusarnold.de>
2019-09-02 16:22:11 +0200

    Update main cpp (Include example of ray tracing for full model)

--- doublebeam-cpp/main.cpp
+++ doublebeam-cpp/main.cpp
@@ -1,4 +1,4 @@
-#include "integration.cpp"
+#include <iomanip>
 #include "model.h"
 #include "printing.h"
 #include "integration.hpp"
@@ -12,20 +12,19 @@ std::ostream& operator<<(std::ostream& os, const RaySegment& segment){
 
 int main() {
     std::vector<Layer> layers{{0,   100, 1000, 1},
-                              {100, 200, 1100, -1}};
+                              {100, 200, 1100, -1},
+                              {200, 300, 1000, 0.5},
+                              {300, 400, 1100, 0},
+                              {400, 500, 1200, -1}};
     auto vm = VelocityModel(layers);
     auto initial_state = init_state(0., 0., 0., vm, math::radians(20.), math::radians(0.));
     std::vector<state_type> values;
     std::vector<double> times;
-
+    std::cout << std::setprecision(16);
     auto krt = KinematicRayTracer(vm);
-    auto ray = krt.trace_ray(initial_state, "R", 1., 1.1);
+    //auto res = measure_runtime([&](){krt.trace_ray(initial_state, "TRT", 1., 1.1);});
+    //std::cout << res << "\n";
+    auto ray = krt.trace_ray(initial_state, "TTTT", 1., 1.1);
     std::cout << ray.segments << "\n";
-    /*
-    auto trace = KinematicRayTracingEquation(vm);
-    auto interface_crossed = InterfaceCrossed(100.);
-    auto res = find_crossing(initial_state, trace, interface_crossed, 0., 1000., 1.);
-    std::cout << res << "\n";
-    */
     return 0;
 }
