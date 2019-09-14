a737960f8e0cf86e7e33193eede4941191200d4d
Darius Arnold <mail@dariusarnold.de>
2019-09-14 17:32:24 +0200

    Add new test assertion that compares floats for equality up to n places

--- doublebeam-cpp/tests/testing_utils.hpp
+++ doublebeam-cpp/tests/testing_utils.hpp
@@ -1,6 +1,7 @@
 #ifndef DOUBLEBEAM_CPP_TESTING_UTILS_HPP
 #define DOUBLEBEAM_CPP_TESTING_UTILS_HPP
 
+#include "utils.hpp"
 #include "gtest/gtest.h"
 
 /**
@@ -10,15 +11,37 @@
  * @param rtol relative tolerance
  * @param atol absolute tolerance
  */
-template<typename T>
+template <typename T>
 ::testing::AssertionResult Close(T actual, T desired, T rtol = 1E-7, T atol = 0) {
     T max_allowed_difference = atol + rtol * std::abs(desired);
-    if (std::abs(actual - desired) <= max_allowed_difference){
+    if (std::abs(actual - desired) <= max_allowed_difference) {
         return ::testing::AssertionSuccess();
     } else {
-        return ::testing::AssertionFailure() << "Actual value " << actual << " different from desired value " << desired
-                                             << " by " << std::abs(actual - desired) << ". Max. allowed difference " << max_allowed_difference;
+        return ::testing::AssertionFailure()
+               << "Actual value " << actual << " different from desired value " << desired << " by "
+               << std::abs(actual - desired) << ". Max. allowed difference "
+               << max_allowed_difference;
     }
 }
 
-#endif //DOUBLEBEAM_CPP_TESTING_UTILS_HPP
+
+/**
+ * EXPECT that actual and desired are the same up to a certain number of places (default 7).
+ * @tparam T Floating point type.
+ * @param actual
+ * @param desired
+ * @param places Both actual and desired are rounded to this number of places after the decimal
+ * point and then compared.
+ * @return
+ */
+template <typename T>
+::testing::AssertionResult AlmostEqual(T actual, T desired, int places = 7) {
+    if (math::round(actual, places) == math::round(desired, places)) {
+        return ::testing::AssertionSuccess();
+    } else {
+        return ::testing::AssertionFailure()
+               << actual << " and " << desired << " differ by more than " << places << " digits.";
+    }
+}
+
+#endif // DOUBLEBEAM_CPP_TESTING_UTILS_HPP
