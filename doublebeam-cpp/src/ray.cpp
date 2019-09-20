a3349904a6563b2deebe978c57c20583577b3519
Darius Arnold <mail@dariusarnold.de>
2019-09-20 13:48:36 +0200

    Add size and index access to Ray

--- doublebeam-cpp/src/ray.cpp
+++ doublebeam-cpp/src/ray.cpp
@@ -8,3 +8,12 @@ std::vector<RaySegment>::iterator Ray::begin() {
 std::vector<RaySegment>::iterator Ray::end() {
     return segments.end();
 }
+size_t Ray::size() {
+    return segments.size();
+}
+RaySegment& Ray::operator[](size_t index) {
+    return segments[index];
+}
+const RaySegment& Ray::operator[](size_t index) const {
+    return segments[index];
+}
--- doublebeam-cpp/src/ray.hpp
+++ doublebeam-cpp/src/ray.hpp
@@ -18,6 +18,21 @@ public:
     std::vector<RaySegment>::iterator begin();
 
     std::vector<RaySegment>::iterator end();
+
+    /**
+     * Return number of ray segments.
+     */
+    size_t size();
+
+    /**
+     * Return const reference to a ray segment.
+     */
+    const RaySegment& operator[](size_t index) const;
+
+    /**
+     * Return reference to a ray segment.
+     */
+    RaySegment& operator[](size_t index);
 };
 
 #endif // DOUBLEBEAM_CPP_RAY_HPP
