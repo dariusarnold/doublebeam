31204cae73eae75d9e07e1131c67252723c6c6db
Darius Arnold <mail@dariusarnold.de>
2019-09-30 16:43:12 +0200

    Update main.cpp

--- doublebeam-cpp/main.cpp
+++ doublebeam-cpp/main.cpp
@@ -1,5 +1,6 @@
 #include "printing.hpp"
 #include "raytracing.hpp"
+#include "timing/timing.hpp"
 #include "twopoint.hpp"
 #include "utils.hpp"
 
@@ -25,6 +26,11 @@ int main() {
     auto vm = read_velocity_file("/home/darius/git/doublebeam/fang2019model.txt");
     auto drt = RayTracer(vm);
     auto initial_state = init_state(0, 0, 0, vm, math::radians(20), 0, 0);
-    auto beam = drt.trace_beam(initial_state, 10, 40, "TTTT");
-    return 0;
+    auto a = std::chrono::high_resolution_clock::now();
+    auto beam0 = drt.trace_beam(initial_state, 10, 40, "TTTT");
+    auto b = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(b - a).count();
+    std::cout << duration << "\n";
+    auto res = measure_runtime([&]() { auto beam = drt.trace_beam(initial_state, 10, 40, "TTTT"); });
+    std::cout << res << std::endl;
 }
