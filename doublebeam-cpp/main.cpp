ca49aba7ae6115c33acb1f69c6dcb1bdbbd0e540
Darius Arnold <mail@dariusarnold.de>
2019-10-21 18:21:36 +0200

    Clean up main.cpp

--- doublebeam-cpp/main.cpp
+++ doublebeam-cpp/main.cpp
@@ -1,4 +1,5 @@
 #include "doublebeam.hpp"
+#include "fft.hpp"
 #include "printing.hpp"
 #include "raytracing.hpp"
 #include "timing/timing.hpp"
@@ -6,7 +7,6 @@
 #include "utils.hpp"
 #include <chrono>
 #include <complex>
-#include "fft.hpp"
 #include <io.hpp>
 
 
@@ -26,50 +26,14 @@ std::ostream& operator<<(std::ostream& os, const Ray& ray) {
     return os;
 }
 
-int nmain() {
-    auto vm = read_velocity_file("/home/darius/git/doublebeam/fang2019model.txt");
-    auto tp = TwoPointRayTracing(vm);
-    for (int i = 0; i < 10; ++i) {
-        auto a = std::chrono::high_resolution_clock::now();
-        tp.trace({50, 10, i*2}, {1, 10+i, 450});
-        auto b = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(b - a).count()
-                  << std::endl;
-    }
-    return 0;
-}
 
-
-
-template <typename T, typename Alloc>
-std::ostream& operator<<(std::ostream& os, const std::vector<T, Alloc>& vec) {
-    for (const auto& el : vec) {
-        os << el << " ";
-    }
-    return os;
-}
-
-
-int main() {
-    FFT fft;
-    std::vector<double> in1{0, 1, 2, 3};
-    std::vector<double> in2{0, 1, 2, 3, 4};
-    std::cerr << in1 << std::endl;
-    std::cerr << in2 << std::endl;
-    auto out1 = fft.execute(in1);
-    auto out2 = fft.execute(in2);
-    std::cerr << out1 << std::endl;
-    std::cerr << out2 << std::endl;
-    return 0;
-}
-/*
 int main() {
     auto vm = read_velocity_file("/home/darius/git/doublebeam/fang2019model.txt");
     auto db = DoubleBeam(vm);
-    auto sources = grid_coordinates(400, 500, 400, 500, 0, 2, 2);
+    auto source_beam_centres = grid_coordinates(400, 500, 400, 500, 0, 2, 2);
     auto targets = grid_coordinates(400, 500, 400, 500, 450, 2, 2);
     FractureParameters fractures(400, 1, 0, 61, 40, 120, 41);
-    db.algorithm(sources, targets, fractures, 10, 40, 0.006);
+    db.algorithm(source_beam_centres, targets,
+                 SeismoData("/home/darius/masterarbeit/output_0degrees"), fractures, 10, 40, 0.006);
     return 0;
 }
-*/
