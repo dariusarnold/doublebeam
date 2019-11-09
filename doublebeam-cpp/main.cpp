67702d284f7940ab1d367e5d2be0a1cf2191878d
Darius Arnold <mail@dariusarnold.de>
2019-11-09 18:38:06 +0100

    Update main.cpp

--- doublebeam-cpp/main.cpp
+++ doublebeam-cpp/main.cpp
@@ -1,13 +1,13 @@
-#include "doublebeam.hpp"
-#include "fft.hpp"
-#include "printing.hpp"
-#include "raytracing.hpp"
-#include "timing/timing.hpp"
-#include "twopoint.hpp"
-#include "utils.hpp"
 #include <chrono>
 #include <complex>
-#include <io.hpp>
+
+#include "doublebeam.hpp"
+#include "eigen_helpers.hpp"
+#include "io.hpp"
+#include "printing.hpp"
+#include "raytracing.hpp"
+#include "twopoint.hpp"
+#include "utils.hpp"
 
 
 std::ostream& operator<<(std::ostream& os, const RaySegment& segment) {
@@ -28,14 +28,21 @@ std::ostream& operator<<(std::ostream& os, const Ray& ray) {
 
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
     auto vm = read_velocity_file("/home/darius/git/doublebeam/fang2019model.txt");
     auto db = DoubleBeam(vm);
-    auto source_beam_centres = seismo::grid_coordinates(400, 500, 400, 500, 0, 1, 1);
-    auto targets = seismo::grid_coordinates(400, 500, 400, 500, 450, 1, 1);
+    auto source_beam_centres = seismo::grid_coordinates(450, 500, 450, 500, 0, 1, 1);
+    // TODO integration doesnt stop exactly at depth, only at layers
+    auto targets = seismo::grid_coordinates(420, 500, 410, 500, 450, 1, 1);
     FractureParameters fractures(400, 1, 0, 2, 40, 120, 3);
+    auto a = std::chrono::high_resolution_clock::now();
     auto result = db.algorithm(source_beam_centres, targets,
                                SeismoData("/home/darius/masterarbeit/output_0degrees"), fractures,
                                10, 40, 0.006);
+    auto b = std::chrono::high_resolution_clock::now();
+    std::cout << "Runtime db : "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(b - a).count() << " ms"
+              << std::endl;
     std::ofstream file{"result.txt"};
     if (file.is_open()) {
         file << result.data;
