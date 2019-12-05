94ab4cdc1a3f64e343d6e5ca2e20301b82740042
Darius Arnold <mail@dariusarnold.de>
2019-12-05 17:21:13 +0100

    Find new output filename instead of overwriting old file

--- doublebeam-cpp/main.cpp
+++ doublebeam-cpp/main.cpp
@@ -43,7 +43,14 @@ int main() {
     auto b = std::chrono::high_resolution_clock::now();
     std::cout << "Runtime db : " << std::chrono::duration_cast<std::chrono::seconds>(b - a).count()
               << " s" << std::endl;
-    std::ofstream file{"result.txt"};
+    std::string result_filename = "result";
+    std::filesystem::path result_path(result_filename + ".txt");
+    int index = 0;
+    while (std::filesystem::exists(result_path)) {
+        ++index;
+        result_path = std::filesystem::path(result_filename + (std::to_string(index)) + ".txt");
+    }
+    std::ofstream file{result_path};
     if (file.is_open()) {
         file << result.data;
     }
