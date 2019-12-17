67aa607f34b21afd44e9e93ce19e24c66d15f428
Darius Arnold <mail@dariusarnold.de>
2019-12-17 19:00:09 +0100

    Add seismogram file extension as parameter to binary conversion script

--- doublebeam-cpp/convert_to_binary.cpp
+++ doublebeam-cpp/convert_to_binary.cpp
@@ -29,9 +29,10 @@ std::vector<fs::path> get_all_files(const fs::path& path, const std::string& fil
 }
 
 
-void convert_content_to_binary(const fs::path& source_dir) {
+void convert_content_to_binary(const fs::path& source_dir,
+                               const std::string& seismogram_file_extension) {
     std::vector<SeismogramFileContent> seismograms;
-    auto files = get_all_files(source_dir, ".txt");
+    auto files = get_all_files(source_dir, seismogram_file_extension);
     if (files.empty()) {
         throw std::length_error("Found no seismogram files in " + source_dir.string());
     }
@@ -41,37 +42,37 @@ void convert_content_to_binary(const fs::path& source_dir) {
 }
 
 
-void convert_all_to_binary(const std::filesystem::path& project_dir) {
-    if (not std::filesystem::exists(project_dir)) {
-        throw std::invalid_argument("Could not find shotdata folder " + project_dir.string() + ".");
+void convert_all_to_binary(const std::filesystem::path& shotdata_path,
+                           const std::string& seismogram_file_extension) {
+    if (not std::filesystem::exists(shotdata_path)) {
+        throw std::invalid_argument("Could not find shotdata folder " + shotdata_path.string() +
+                                    ".");
     }
-    if (not std::filesystem::is_directory(project_dir)) {
-        throw std::invalid_argument(project_dir.string() + " is not a directory.");
+    if (not std::filesystem::is_directory(shotdata_path)) {
+        throw std::invalid_argument(shotdata_path.string() + " is not a directory.");
     }
-    std::cout << "Converting all files in " << project_dir << std::endl;
-    auto paths = get_all_directories(project_dir);
+    std::cout << "Converting all files in " << shotdata_path << std::endl;
+    auto paths = get_all_directories(shotdata_path);
     std::sort(paths.begin(), paths.end());
     auto i = 0;
     for (const auto& source_dir : paths) {
         std::cout << ++i << "/" << paths.size() << ": " << source_dir << std::endl;
-        convert_content_to_binary(source_dir);
+        convert_content_to_binary(source_dir, seismogram_file_extension);
     }
 }
 
 
 int main(int argc, char* argv[]) {
-    if (argc == 1) {
-        std::cerr << "Specify path to shotdata folder as command line argument.\n";
-        exit(-1);
-    }
-    if (argc > 2) {
-        std::cerr << "Too many arguments.\n";
+    if (argc != 3) {
+        std::cerr << "Specify path to shotdata folder as first positional argument and seismogram "
+                     "file extension as second positional argument.\n";
         exit(-1);
     }
     std::ios::sync_with_stdio(false);
     std::filesystem::path p(argv[1]);
+    std::string seismogram_file_extension(argv[2]);
     auto a = std::chrono::high_resolution_clock::now();
-    convert_all_to_binary(p);
+    convert_all_to_binary(p, seismogram_file_extension);
     auto b = std::chrono::high_resolution_clock::now();
     std::cout << "Took " << std::chrono::duration_cast<std::chrono::seconds>(b - a).count()
               << " s\n";
