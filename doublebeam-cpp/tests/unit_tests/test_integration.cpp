82a0b75bcaa2cc68d57d28af23241463caf5a2cf
Darius Arnold <mail@dariusarnold.de>
2019-12-20 19:12:03 +0100

    Adapt snells law tests to new function interface

--- doublebeam-cpp/tests/unit_tests/test_integration.cpp
+++ doublebeam-cpp/tests/unit_tests/test_integration.cpp
@@ -4,15 +4,18 @@
 #include "raytracing_types.hpp"
 
 TEST(SnellsLaw, CompareWithPython) {
-    double_t v_before = 2000, v_after = 2200;
-    double px_in = 0.7248451923249044, py_in = 0.205219943364641, pz_in = 0.969149611692033;
-    double px_out = 0.7248451923249044, py_out = 0.205219943364641, pz_out = 0.9691495893072383;
+    Velocity v_before(2000);
+    Velocity v_after(2200);
+    Slowness p_in(0.7248451923249044_second_per_meter, 0.205219943364641_second_per_meter,
+                  0.969149611692033_second_per_meter);
+    Slowness p_out(0.7248451923249044_second_per_meter, 0.205219943364641_second_per_meter,
+                   0.9691495893072383_second_per_meter);
 
-    auto [px, py, pz] = snells_law(px_in, py_in, pz_in, v_before, v_after, WaveType::Transmitted);
-    EXPECT_FALSE(pz == pz_in);
-    EXPECT_DOUBLE_EQ(px, px_out);
-    EXPECT_DOUBLE_EQ(py, py_out);
-    EXPECT_DOUBLE_EQ(pz, pz_out);
+    auto [px, py, pz] = snells_law(p_in, v_before, v_after, WaveType::Transmitted);
+    EXPECT_FALSE(pz == p_in.pz);
+    EXPECT_DOUBLE_EQ(px.get(), p_out.px.get());
+    EXPECT_DOUBLE_EQ(py.get(), p_out.py.get());
+    EXPECT_DOUBLE_EQ(pz.get(), p_out.pz.get());
 }
 
 TEST(SnellsLaw, ReflectedRay) {
@@ -21,9 +24,11 @@ TEST(SnellsLaw, ReflectedRay) {
      * for the reflected wave at the interface in the same layer), only the sign of pz is
      * changed.
      */
-    double px_in = 0.7248451923249044, py_in = 0.205219943364641, pz_in = 0.969149611692033;
-    auto [px, py, pz] = snells_law(px_in, py_in, pz_in, 1000, 2000, WaveType::Reflected);
-    EXPECT_DOUBLE_EQ(px, px_in);
-    EXPECT_DOUBLE_EQ(py, py_in);
-    EXPECT_DOUBLE_EQ(pz, -pz_in);
+    Slowness p_in(0.7248451923249044_second_per_meter, 0.205219943364641_second_per_meter,
+                  0.969149611692033_second_per_meter);
+    auto [px, py, pz] =
+        snells_law(p_in, 1000_meter_per_second, 2000_meter_per_second, WaveType::Reflected);
+    EXPECT_DOUBLE_EQ(px.get(), p_in.px.get());
+    EXPECT_DOUBLE_EQ(py.get(), p_in.py.get());
+    EXPECT_DOUBLE_EQ(pz.get(), -p_in.pz.get());
 }
