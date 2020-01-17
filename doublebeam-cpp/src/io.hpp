dbaee16f0c822b52e9ae3b76e0acddeac2a58da4
Darius Arnold <mail@dariusarnold.de>
2020-01-17 11:16:19 +0100

    Bugfix: Options passed from command line were not saved in result

--- doublebeam-cpp/src/io.hpp
+++ doublebeam-cpp/src/io.hpp
@@ -152,45 +152,11 @@ inline bool contains(const std::string& string, std::string_view search_term) {
 }
 
 struct SourceBeamCenterParams {
-    inline void init_from_file(std::ifstream& config_file) {
-        std::string line;
-        while (std::getline(config_file, line)) {
-            if (contains(line, "x0")) {
-                x0.get() = extract_double(line);
-                continue;
-            }
-            if (contains(line, "x1")) {
-                x1.get() = extract_double(line);
-                continue;
-            }
-            if (contains(line, "y0")) {
-                y0.get() = extract_double(line);
-                continue;
-            }
-            if (contains(line, "y1")) {
-                y1.get() = extract_double(line);
-                continue;
-            }
-            if (contains(line, "z")) {
-                z.get() = extract_double(line);
-                continue;
-            }
-            if (contains(line, "num_x")) {
-                num_x = extract_int(line);
-                continue;
-            }
-            if (contains(line, "num_y")) {
-                num_y = extract_int(line);
-                continue;
-            }
-            break;
-        }
-    }
     Meter x0, x1, y0, y1, z;
     int num_x, num_y;
 
     friend std::ostream& operator<<(std::ostream& os, const SourceBeamCenterParams& sbc_params) {
-        fmt::print(os, "[source beam centers]\n");
+        fmt::print(os, "[source_beam_centers]\n");
         fmt::print(os, "x0 = {}\nx1 = {}\ny0 = {}\ny1 = {}\nz = {}\nnum_x = {}\nnum_y = {}\n\n",
                    sbc_params.x0.get(), sbc_params.x1.get(), sbc_params.y0.get(),
                    sbc_params.y1.get(), sbc_params.z.get(), sbc_params.num_x, sbc_params.num_y);
@@ -199,28 +165,6 @@ struct SourceBeamCenterParams {
 };
 
 struct FractureParams {
-    inline void init_from_file(std::ifstream& config_file) {
-        std::string line;
-        while (std::getline(config_file, line)) {
-            if (contains(line, "num_orientations")) {
-                num_orientations = extract_int(line);
-                continue;
-            }
-            if (contains(line, "spacing_min")) {
-                spacings_min = Meter(extract_double(line));
-                continue;
-            }
-            if (contains(line, "spacing_max")) {
-                spacings_max = Meter(extract_double(line));
-                continue;
-            }
-            if (contains(line, "num_spacings")) {
-                num_spacings = extract_int(line);
-                continue;
-            }
-            break;
-        }
-    }
     math::Vector2 phi_hat{1, 0};
     int num_orientations;
     Meter spacings_min, spacings_max;
@@ -238,30 +182,8 @@ struct FractureParams {
 };
 
 struct BeamParams {
-    inline void init_from_file(std::ifstream& config_file) {
-        std::string line;
-        while (std::getline(config_file, line)) {
-            if (contains(line, "width")) {
-                width = Meter(extract_double(line));
-                continue;
-            }
-            if (contains(line, "frequency")) {
-                frequency = AngularFrequency(2 * M_PI * extract_double(line));
-                continue;
-            }
-            if (contains(line, "window_length")) {
-                window_length = Second(extract_double(line));
-                continue;
-            }
-            if (contains(line, "max_stacking_distance")) {
-                max_stacking_distance = Meter(extract_double(line));
-                continue;
-            }
-            break;
-        }
-    }
     Meter width;
-    AngularFrequency frequency;
+    Frequency frequency;
     Second window_length;
     Meter max_stacking_distance;
 
@@ -269,27 +191,13 @@ struct BeamParams {
         fmt::print(os, "[beam]\n");
         fmt::print(os,
                    "width = {}\nfrequency = {}\nwindow_length = {}\nmax_stacking_distance = {}\n\n",
-                   beam_params.width.get(), angular_to_hertz(beam_params.frequency).get(),
+                   beam_params.width.get(), beam_params.frequency.get(),
                    beam_params.window_length.get(), beam_params.max_stacking_distance.get());
         return os;
     }
 };
 
 struct SeismoDataParams {
-    inline void init_from_file(std::ifstream& config_file) {
-        std::string line;
-        while (std::getline(config_file, line)) {
-            if (contains(line, "path")) {
-                data_path = extract_path(line);
-                continue;
-            }
-            if (contains(line, "model")) {
-                velocity_model_path = extract_path(line);
-                continue;
-            }
-            break;
-        }
-    }
     std::filesystem::path data_path;
     std::filesystem::path velocity_model_path;
 
@@ -302,26 +210,6 @@ struct SeismoDataParams {
 };
 
 struct TargetParams {
-    inline void init_from_file(std::ifstream& config_file) {
-        std::string line;
-        double x = 0, y = 0, z = 0;
-        while (std::getline(config_file, line)) {
-            if (contains(line, "x")) {
-                x = extract_number<double>(line);
-                continue;
-            }
-            if (contains(line, "y")) {
-                y = extract_number<double>(line);
-                continue;
-            }
-            if (contains(line, "z")) {
-                z = extract_number<double>(line);
-                continue;
-            }
-            break;
-        }
-        position = {Meter(x), Meter(y), Meter(z)};
-    }
     Position position;
 
     friend std::ostream& operator<<(std::ostream& os, const TargetParams& target_params) {
@@ -332,10 +220,7 @@ struct TargetParams {
     }
 };
 
-/*
- * I am sorry about the mess of this. Especially parsing the options is dirty and brittle.
- * I should have used a library for this instead of DIY.
- */
+
 struct DoubleBeamOptions {
     SeismoDataParams seismo_data_params{};
     TargetParams target{};
@@ -349,33 +234,5 @@ struct DoubleBeamOptions {
         return os;
     }
 };
-
-inline DoubleBeamOptions read_config_file(const std::filesystem::path& config_path) {
-    std::ifstream config_file{config_path};
-    if (not config_file.is_open()) {
-        throw std::runtime_error("Failed to open config file " + config_path.string());
-    }
-    std::string line;
-    DoubleBeamOptions options;
-    while (std::getline(config_file, line)) {
-        if (contains(line, "[data]")) {
-            options.seismo_data_params.init_from_file(config_file);
-        }
-        if (contains(line, "[target]")) {
-            options.target.init_from_file(config_file);
-        }
-        if (contains(line, "[source beam centers]")) {
-            options.sbc_params.init_from_file(config_file);
-        }
-        if (contains(line, "[fractures]")) {
-            options.fracture_params.init_from_file(config_file);
-        }
-        if (contains(line, "[beam]")) {
-            options.beam_params.init_from_file(config_file);
-        }
-    }
-    return options;
-}
-
 
 #endif // DOUBLEBEAM_CPP_IO_HPP
