8abe12aa36f228cdb0587de8aa75f87fb930341b
Darius Arnold <mail@dariusarnold.de>
2020-02-22 22:10:18 +0100

    Improve plotting code by adding comments

--- doublebeam-cpp/scripts/plot_results.py
+++ doublebeam-cpp/scripts/plot_results.py
@@ -15,36 +15,43 @@ def plot_scattering_coefficient(data: np.ndarray, options: Options, fname):
     axis gives angle samples, second axis gives fracture spacing.
     :param min_spacing: min fracture spacing
     :param max_spacing: max fracture spacing
+    :param fname: Filename for output file, pdf and png will be saved.
     :return:
     """
     fig, ax = plt.subplots(subplot_kw={"polar": True}, figsize=(6, 6), dpi=210)
-    angles = np.radians(np.linspace(0, 180, data.shape[1] + 1))
     # +1 otherwise last column of data will be ignored
+    angles = np.radians(np.linspace(0, 180, data.shape[1] + 1))
     radii = np.linspace(options.fracture_params.spacing_min, options.fracture_params.spacing_max,
                         data.shape[0] + 1)
+    # vmin = 0 to force full color scale from 0 to max
     im = ax.pcolormesh(angles, radii, data, vmin=0)
-    # add grid
+    # set radial ticks to fracture spacings
+    num_of_ticks = 5
     major_ticks_radius = np.linspace(options.fracture_params.spacing_min,
-                                     options.fracture_params.spacing_max, 5)
+                                     options.fracture_params.spacing_max, num_of_ticks)
     ax.set_rticks(major_ticks_radius)
-    major_ticks_angle = np.linspace(0, np.pi, 5)
+    # set angular ticks to 0°-180°
+    major_ticks_angle = np.linspace(0, np.pi, num_of_ticks)
     ax.set_xticks(major_ticks_angle)
-    # add label to radial axis
+    # add fracture spacing label to radial axis
     rlabel_pos = ax.get_rlabel_position()
     ax.text(np.radians(rlabel_pos - 40), ax.get_rmax() / 1.45, "Fracture spacing (m)", rotation=0,
             ha="center",
             va="center")
+    # enable grid
     ax.grid(color="white")
     # limit to half circle
     ax.set_thetamax(180)
     # create inner "cutout" by setting origin and min/max for radial axis
     ax.set_rorigin(10)
     ax.set_ylim(options.fracture_params.spacing_min, options.fracture_params.spacing_max)
-    cbar_format = ticker.ScalarFormatter()
     # force scientific notation with exponent at top
+    cbar_format = ticker.ScalarFormatter()
     cbar_format.set_powerlimits((-1, 1))
+    # add colorbar and label it
     cbar = fig.colorbar(im, ax=ax, shrink=.5, pad=.08, aspect=15, format=cbar_format)
     cbar.set_label(r"$|\sigma|$")
+    # plot values used to generate data in a box on the figure
     textbox_content = "\n".join((fr"$\omega_s = {options.beam_params.source_frequency}$ Hz",
                                  fr"$\omega_r = {options.beam_params.reference_frequency}$ Hz",
                                  fr"$w = {options.beam_params.width} $ m",
@@ -54,14 +61,14 @@ def plot_scattering_coefficient(data: np.ndarray, options: Options, fname):
                                  fr"    $x = {options.source_beam_centers.x0:.0f}$ ... ${options.source_beam_centers.x1:.0f}$ m",
                                  fr"    $y = {options.source_beam_centers.y0:.0f}$ ... ${options.source_beam_centers.y1:.0f}$ m"
                                  ))
-    path_textbox_content = "\n".join((f"{options.data.model_path}",
-                                      f"{options.data.data_path}"))
     box_properties = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
     ax.text(0., 0.15, textbox_content, transform=ax.transAxes, fontsize=10, verticalalignment="top",
             bbox=box_properties)
+    # add path to data as text above the plot
+    path_textbox_content = "\n".join((f"{options.data.model_path}",
+                                      f"{options.data.data_path}"))
     ax.text(-0.1, 1, path_textbox_content, transform=ax.transAxes, fontsize=7)
-    ticks = list(cbar.get_ticks())
-    # cbar.set_ticks([np.min(data), np.max(data)] + ticks)
+    # set title to target position
     title = ax.set_title(f"Target ({options.target.x} m, {options.target.y} m, {options.target.z} m)")
     title.set_position((.5, .85))
     # plt.show()
@@ -96,6 +103,7 @@ def main():
         x_dense = np.linspace(0, x[-1], int(len(x)*scale_factor))
         y_dense = np.linspace(0, y[-1], int(len(y)*scale_factor))
         data = interpolator(x_dense, y_dense)
+        
     plot_scattering_coefficient(data, options, fname)
 
 
